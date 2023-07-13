a56537438c14010808e2442f4fe8cedfcb9035fe
MTayeb93 <m.tayeb993@gmail.com>
2023-07-13 11:43:32 +0300

    add the popular jobs list and get it ready to be fetched from api

--- app/_layout.js
+++ app/_layout.js
@@ -9,15 +9,16 @@ const Layout = () => {
   const [fontsLoaded] = useFonts({
     DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
     DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-    DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
   })
+ 
   const onLayoutRootView = useCallback(async () => {
-    if(fontsLoaded) {
+    if (fontsLoaded) {
       await SplashScreen.hideAsync()
     }
   }, [fontsLoaded])
 
-  if(!fontsLoaded) return null
+  if (!fontsLoaded) return null
 
   return <Stack onLayout={onLayoutRootView} />
 }
